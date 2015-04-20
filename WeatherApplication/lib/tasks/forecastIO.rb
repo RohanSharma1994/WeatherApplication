8ac83f889e0251a3a422ff3cf02e2c3a8de2aecb
RohanSharma1994 <RohanSharma1994@hotmail.com>
2015-04-20 22:17:04 +1000

    The forecastIO data is also now stored in the database after being retrieved by the API

--- WeatherApplication/lib/tasks/forecastIO.rb
+++ WeatherApplication/lib/tasks/forecastIO.rb
@@ -7,18 +7,11 @@ require 'open-uri'
 BASE_URL = 'https://api.forecast.io/forecast'
 API_KEY = '8853c97154dd2a79c781123b4f3b39c5'
 UNITS = '?units=ca'
+SOURCE = 'Forecast IO'
 
-# Get all the weather stations from the database
-weather_stations = WeatherStation.all
-
-# For each of the weather stations in the database
-# get the current prediction from forecast.io
-for weather_station in weather_stations
-	# Get the lat,lon location 
-	lat_lon= "#{weather_station.lat},#{weather_station.lon}"
-	link = "#{BASE_URL}/#{API_KEY}/#{lat_lon}#{UNITS}"
-	# Get the forecast
-	forecast = JSON.parse(open(link).read)
+# A function which gets the time
+# since 9am.
+def get_time_since_9am()
 	time = Time.new
 	if(time.hour > 9) 
 		time_since_9am = time.hour - 9 + time.min.to_f/60 + time.sec.to_f/3600
@@ -27,6 +20,32 @@ for weather_station in weather_stations
 	else
 		time_since_9am = (24-9)+time.hour+time.min.to_f/60+time.sec.to_f/3600
 	end
-	# Print the weather
-	puts "Name:#{weather_station.name}|Temp:#{forecast['currently']['temperature']}|Dew_Point:#{forecast['currently']['dewPoint']}|Wind_Speed:#{forecast['currently']['windSpeed']}|Wind_Direction:#{forecast['currently']['windBearing']}|Rainfall_amount:#{forecast['currently']['precipIntensity']*time_since_9am}"
+	return time_since_9am
+end
+
+# Get all the weather stations from the database
+weather_stations = WeatherStation.all
+
+# For each of the weather stations in the database
+# get the current prediction from forecast.io
+for weather_station in weather_stations
+	# Create a daily observation for this weather station
+	daily_observation = weather_station.daily_observations.create(source:SOURCE, date_time:Time.now)
+	# Get the lat,lon location 
+	lat_lon= "#{weather_station.lat},#{weather_station.lon}"
+	link = "#{BASE_URL}/#{API_KEY}/#{lat_lon}#{UNITS}"
+	# Get the forecast
+	forecast = JSON.parse(open(link).read)
+	# Calculate the time since 9am
+	time_since_9am = get_time_since_9am
+	# Parse the data
+	temp = forecast['currently']['temperature']
+	dew_point = forecast['currently']['dewPoint']
+	wind_spd = forecast['currently']['windSpeed']
+	wind_dir = forecast['currently']['windBearing']
+	rainfall_amount = forecast['currently']['precipIntensity']*time_since_9am
+	# Store the data for this daily observation in the database
+	daily_observation.temperature_observation.create(current_temperature:temp, dew_point:dew_point)
+	daily_observation.wind_observation.create(wind_speed:wind_spd, wind_direction:wind_dir)
+	daily_observation.rain_observation.create(rainfall_amount:rainfall_amount)	
 end
